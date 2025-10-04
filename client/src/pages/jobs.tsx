55bb68ab8b06996db688b9806e908138927e6347
GreensandGrains <greensandgrains2025@gmail.com>
2025-10-04 18:27:48 +0530

    Update jobs.tsx

--- client/src/pages/jobs.tsx
+++ client/src/pages/jobs.tsx
@@ -134,7 +134,6 @@ export default function Jobs() {
           )}
           </div>
         </div>
-      </section>
       
       <div className="container mx-auto px-4 py-8">
         <div className="max-w-7xl mx-auto">
